0b101225489f4dcc7046784c57a8915decb89804
Sashinth16 <sashinth5656@gmail.com>
2026-06-23 13:15:04 +0530

    Create and_gate.v

---
+++ rtl/and_gate.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 20.06.2026 07:50:19
+// Design Name: 
+// Module Name: and_gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module and_gate(input a,input b,output y);
+
+assign y = a&b;
+
+endmodule
